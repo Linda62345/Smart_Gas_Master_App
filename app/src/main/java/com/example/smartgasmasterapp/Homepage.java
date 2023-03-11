ea27a2cde7ea6d274d4efa761fe06c946173fee4
linda <linda62345@gmail.com>
2023-03-11 16:13:52 +0800

    logout, homepage username

--- app/src/main/java/com/example/smartgasmasterapp/Homepage.java
+++ app/src/main/java/com/example/smartgasmasterapp/Homepage.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.smartgasmasterapp.ui.login.LoginActivity;
 
@@ -30,6 +31,8 @@ public class Homepage extends AppCompatActivity {
     private Button scanOrder;
     private String email;
     public int Worker_ID;
+    public Button logout;
+    public TextView name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +41,19 @@ public class Homepage extends AppCompatActivity {
 
         Objects.requireNonNull(getSupportActionBar()).hide();
 
+        logout = findViewById(R.id.logout);
+        name = findViewById(R.id.changeable_username);
         editProfile = findViewById(R.id.edit_personal_info_button);
         orderList = findViewById(R.id.orderList_page_button);
         scanOrder = findViewById(R.id.open_scanner_button);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Homepage.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
         editProfile.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,6 +79,57 @@ public class Homepage extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        LoginActivity loginActivity = new LoginActivity();
+        Worker_ID = loginActivity.getWorkerID();
+        Thread thread = new Thread(new Runnable() {
 
+            @Override
+            public void run() {
+                try  {
+                    WorkerName();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+
+
+    }
+    public void WorkerName(){
+        try{
+            String Showurl = "http://10.0.2.2/SQL_Connect/Show_Worker_Profile.php";
+            URL url = new URL(Showurl);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("POST");
+            httpURLConnection.setDoOutput(true);
+            httpURLConnection.setDoInput(true);
+            OutputStream outputStream = httpURLConnection.getOutputStream();
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+            String post_data = URLEncoder.encode("id", "UTF-8") + "=" + URLEncoder.encode(String.valueOf(Worker_ID), "UTF-8");
+            bufferedWriter.write(post_data);
+            bufferedWriter.flush();
+            bufferedWriter.close();
+            outputStream.close();
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+            String result = "";
+            String line = "";
+
+            while ((line = bufferedReader.readLine()) != null) {
+                result += line;
+            }
+            bufferedReader.close();
+            inputStream.close();
+            httpURLConnection.disconnect();
+            Log.i("result", "["+result+"]");
+            JSONObject responseJSON = new JSONObject(result);
+            String Worker_Name = responseJSON.getString("Worker_Name");
+            name.setText(Worker_Name);
+        }
+        catch(Exception e){
+            Log.i("Worker name",e.toString());
+        }
     }
 }
