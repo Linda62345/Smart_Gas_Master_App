62af58619fc4e00da9b0ea043caf28a5107a5c69
humankill <109306034@g.nccu.edu.tw>
2023-05-04 20:13:45 +0800

    殘氣結算

---
+++ app/src/main/java/com/example/smartgasmasterapp/GasAccumulation.java
@@ -0,0 +1,14 @@
+package com.example.smartgasmasterapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GasAccumulation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gas_accumulation);
+    }
+}
