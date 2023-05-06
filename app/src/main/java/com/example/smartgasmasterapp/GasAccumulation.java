1e9fb9a06bef953684d3f5a4359ed997c549df21
humankill <109306034@g.nccu.edu.tw>
2023-05-07 07:23:46 +0800

    殘氣結算

--- app/src/main/java/com/example/smartgasmasterapp/GasAccumulation.java
+++ app/src/main/java/com/example/smartgasmasterapp/GasAccumulation.java
@@ -2,13 +2,34 @@ package com.example.smartgasmasterapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Objects;
 
 public class GasAccumulation extends AppCompatActivity {
+
+    private Button next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gas_accumulation);
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+
+        next = findViewById(R.id.confirm_NewScan_button);
+
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GasAccumulation.this, ScanNewQRCode.class);
+                startActivity(intent);
+            }
+        });
     }
 }
